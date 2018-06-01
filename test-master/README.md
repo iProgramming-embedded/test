f34065fc46decc568470820b4fdf1e283805a924
LeonLinuxNerd <1206225794@qq.com>
2018-06-01 23:44:27 +0800

    error

--- test-master/README.md
+++
@@ -1,3 +0,0 @@
-# test
-test for a noob
-Github上建立的第一个库，后续考虑更新C++的学习内容^_^
--- test-master/git code.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
- git add .
- git commit -m "first"
- git remote rm origin
- git remote add origin git@github.com:LeonLinuxNerd/test.git
- git pull origin master
- git push -u origin master
